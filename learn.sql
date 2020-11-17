3d36cfda16310f3436d26a77724fc3e66eaae9b1
sraban <sraban_pvt@yahoo.com>
2020-11-17 13:17:06 +0530

    Add files via upload

---
+++ learn.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `customers` (                                                                                                                                                                                                                      
+             `id` int NOT NULL AUTO_INCREMENT,                                                                                                                                                                                                             
+             `email` varchar(255) NOT NULL,                                                                                                                                                                                                                
+             `name` varchar(255) NOT NULL,                                                                                                                                                                                                                 
+             `active` tinyint(1) DEFAULT '0',                                                                                                                                                                                                              
+             PRIMARY KEY (`id`)                                                                                                                                                                                                                            
+           ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
